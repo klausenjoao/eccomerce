f294c609557a025181919197f925ee8f2e536115
João Vitor Dos Santos Klausen <joaovitor_klausen@hotmail.com>
2023-10-12 18:12:19 -0300

    Commit 1210

--- src/utlidades.js
+++ src/utlidades.js
@@ -81,3 +81,57 @@ export const catalogo = [
   export function lerLocalStorage(chave, informacao){
     return JSON.parse(localStorage.getItem(chave));
   }
+
+  function desenharProdutoNoCarrinho(idProduto) {
+    const produto = catalogo.find((p) => p.id === idProduto);
+    const containerProdutosCarrinho =
+      document.getElementById("produtos-carrinho");
+  
+    const elementoArticle = document.createElement("article");
+    const articleClasses = [
+      "flex",
+      "p-1",
+      "relative",
+      "border-slate-950",
+      "border",
+      "border-black",
+    ];
+  
+    for (const articleClass of articleClasses) {
+      elementoArticle.classList.add(articleClass);
+    }
+  
+    const cartaoProdutoCarrinho = `<button id="remover-item-${produto.id}"  class="absolute  top-0 right-2">
+        <i class="fa-solid fa-trash text-slate-500 hover:text-slate-800"></i>
+      </button>
+      <img
+      src="./assets/${produto.imagem}"
+      alt="Carrinho: ${produto.nome}"
+      class="h-24 rounded-lg" />
+      <div class="p-2 flex flex-col justify-between">
+        <p class="text-slate-900 text-small">${produto.nome}</p>
+        <p class="text-slate-400 text-xs">Tamanho: M</p>
+        <p class="text-green-700 text-lg">$${produto.preco}</p>
+      </div>
+      <div class='flex text-slate-950 items-end absolute bottom-0 right-2 text-lg'>
+        <button id='decrementar-produto-${produto.id}' class=''>-</button>
+        <p id='quantidade-${produto.id}' class='ml-2'>${idsProdutoCarrinhoComQuantidade[produto.id]
+      }
+        </p>
+        <button id='incrementar-produto-${produto.id}'  class='ml-2'>+</button>
+      </div>`;
+  
+    elementoArticle.innerHTML = cartaoProdutoCarrinho;
+    containerProdutosCarrinho.appendChild(elementoArticle);
+  
+    document
+      .getElementById(`decrementar-produto-${produto.id}`)
+      .addEventListener("click", () => decrementarQuantidadeProduto(idProduto));
+    document
+      .getElementById(`incrementar-produto-${produto.id}`)
+      .addEventListener("click", () => incrementarQuantidadeProduto(idProduto));
+  
+      document
+      .getElementById(`remover-item-${produto.id}`)
+      .addEventListener("click", () => removerDoCarrinho(idProduto));
+  }
